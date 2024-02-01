e3f5af806a82083ec92ecdb93359e8ff3c7c1c0c
RedouaneJnah98 <redouanejnah2000@gmail.com>
2024-02-01 22:36:32 +0100

    Setup project files

---
+++ MyBGList/Models/BoardGame.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBGList.Models;
+
+[Table("BoardGames")]
+public class BoardGame
+{
+    [Key] [Required] public int Id { get; set; }
+
+    [Required] [MaxLength(200)] public string Name { get; set; } = null!;
+
+    [Required] public int Year { get; set; }
+
+    [Required] public int MinPlayers { get; set; }
+
+    [Required] public int MaxPlayers { get; set; }
+
+    [Required] public int PlayTime { get; set; }
+
+    [Required] public int MinAge { get; set; }
+
+    [Required] public int UsersRated { get; set; }
+
+    [Required] [Precision(4, 2)] public decimal RatingAverage { get; set; }
+
+    [Required] public int BGGRank { get; set; }
+
+    [Required] [Precision(4, 2)] public decimal ComplexityAverage { get; set; }
+
+    [Required] public int OwnedUsers { get; set; }
+
+    [Required] public DateTime CreatedDate { get; set; }
+
+    [Required] public DateTime LastModifiedDate { get; set; }
+
+    public ICollection<BoardGames_Domains>? BoardGames_Domains { get; set; }
+
+    public ICollection<BoardGames_Mechanics>? BoardGames_Mechanics { get; set; }
+
+    [MaxLength(200)] public string? AlternateNames { get; set; }
+
+    [MaxLength(200)] public string? Designer { get; set; }
+
+    [Required] public int Flags { get; set; }
+
+    [Required] public int PublisherId { get; set; }
+}
---
+++ MyBGList/Models/Publisher.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyBGList.Models;
+
+[Table("Publishers")]
+public class Publisher
+{
+    [Key] [Required] public int Id { get; set; }
+
+    [Required] [MaxLength(200)] public string Name { get; set; }
+
+    [Required] public DateTime CreatedDate { get; set; }
+
+    [Required] public DateTime LastModified { get; set; }
+
+    public ICollection<BoardGame>? BoardGames { get; set; }
+}
