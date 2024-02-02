7b8b0dac6aef700f6aae68c810bedd3d7da7714c
RedouaneJnah98 <redouanejnah2000@gmail.com>
2024-02-02 23:19:53 +0100

    return data from BoardGamesController.cs

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -1,50 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyBGList.DTO;
 using MyBGList.Models;
 
-namespace MyBGList.Controllers
+namespace MyBGList.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class BoardGamesController : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class BoardGamesController : ControllerBase
+    private readonly ApplicationDbContext _context;
+    private readonly ILogger<BoardGamesController> _logger;
+
+    public BoardGamesController(
+        ApplicationDbContext context,
+        ILogger<BoardGamesController> logger)
     {
-        private readonly ILogger<BoardGamesController> _logger;
+        _context = context;
+        _logger = logger;
+    }
 
-        public BoardGamesController(ILogger<BoardGamesController> logger)
-        {
-            _logger = logger;
-        }
+    [HttpGet(Name = "GetBoardGames")]
+    [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
+    public async Task<RestDTO<BoardGame[]>> Get()
+    {
+        var query = _context.BoardGames;
 
-        [HttpGet(Name = "GetBoardGames")]
-        [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
-        public RestDTO<BoardGame[]> Get()
+        return new RestDTO<BoardGame[]>
         {
-            return new RestDTO<BoardGame[]>()
+            Data = await query.ToArrayAsync(),
+            Links = new List<LinkDTO>
             {
-                Data = new [] {
-            new BoardGame() {
-                Id = 1,
-                Name = "Axis & Allies",
-                Year = 1981
-            },
-            new BoardGame() {
-                Id = 2,
-                Name = "Citadels",
-                Year = 2000
-            },
-            new BoardGame() {
-                Id = 3,
-                Name = "Terraforming Mars",
-                Year = 2016
+                new(
+                    Url.Action(null, "BoardGames", null, Request.Scheme)!,
+                    "self",
+                    "GET")
             }
-        },
-                Links = new List<LinkDTO> {
-            new LinkDTO(
-                Url.Action(null, "BoardGames", null, Request.Scheme)!,
-                "self",
-                "GET"),
-        }
-            };
-        }
+        };
     }
 }
