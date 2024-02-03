b921d62a0e3e6afc909b3f3e383149b28483a678
RedouaneJnah98 <redouanejnah2000@gmail.com>
2024-02-03 21:16:04 +0100

    Add paging to API.

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -22,17 +22,28 @@ public class BoardGamesController : ControllerBase
 
     [HttpGet(Name = "GetBoardGames")]
     [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
-    public async Task<RestDTO<BoardGame[]>> Get()
+    public async Task<RestDTO<BoardGame[]>> Get(
+        int pageIndex = 0,
+        int pageSize = 10)
     {
-        var query = _context.BoardGames;
+        var query = _context.BoardGames
+            .Skip(pageIndex * pageSize)
+            .Take(pageSize);
 
         return new RestDTO<BoardGame[]>
         {
             Data = await query.ToArrayAsync(),
+            PageIndex = pageIndex,
+            PageSize = pageSize,
+            RecordCount = await _context.BoardGames.CountAsync(),
             Links = new List<LinkDTO>
             {
                 new(
-                    Url.Action(null, "BoardGames", null, Request.Scheme)!,
+                    Url.Action(
+                        null,
+                        "BoardGames",
+                        new { pageIndex, pageSize },
+                        Request.Scheme)!,
                     "self",
                     "GET")
             }
