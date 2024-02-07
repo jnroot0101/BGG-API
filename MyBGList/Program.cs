fe63f599cb604939bb5b313696f81b769e943063
RedouaneJnah98 <redouanejnah2000@gmail.com>
2024-02-07 21:13:08 +0100

    Disable the automatic ModelState validation feature.

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -8,21 +8,34 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddCors(options => {
-    options.AddDefaultPolicy(cfg => {
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(cfg =>
+    {
         cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
         cfg.AllowAnyHeader();
         cfg.AllowAnyMethod();
     });
-    options.AddPolicy(name: "AnyOrigin",
-        cfg => {
+    options.AddPolicy("AnyOrigin",
+        cfg =>
+        {
             cfg.AllowAnyOrigin();
             cfg.AllowAnyHeader();
             cfg.AllowAnyMethod();
         });
-    });
+});
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ModelBindingMessageProvider.SetValueIsInvalidAccessor(
+        x => $"The value {x} is invalid.");
+    options.ModelBindingMessageProvider.SetValueMustBeANumberAccessor(
+        x => $"The field {x} must be a number.");
+    options.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor(
+        (x, y) => $"The value {x} is not valid for {y}.");
+    options.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(
+        () => "A value is required.");
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -34,7 +47,12 @@ builder.Services.AddSwaggerGen(options =>
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection"))
-    );
+);
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 
 var app = builder.Build();
 
@@ -58,27 +76,26 @@ app.UseAuthorization();
 
 // Minimal API
 app.MapGet("/error",
-    [EnableCors("AnyOrigin")]
-    [ResponseCache(NoStore = true)] () =>
-    Results.Problem());
+    [EnableCors("AnyOrigin")] [ResponseCache(NoStore = true)]
+    () =>
+        Results.Problem());
 
 app.MapGet("/error/test",
-    [EnableCors("AnyOrigin")]
-    [ResponseCache(NoStore = true)] () =>
-    { throw new Exception("test"); });
+    [EnableCors("AnyOrigin")] [ResponseCache(NoStore = true)]
+    () => { throw new Exception("test"); });
 
 app.MapGet("/cod/test",
-    [EnableCors("AnyOrigin")]
-    [ResponseCache(NoStore = true)] () =>
-    Results.Text("<script>" +
-        "window.alert('Your client supports JavaScript!" +
-        "\\r\\n\\r\\n" +
-        $"Server time (UTC): {DateTime.UtcNow.ToString("o")}" +
-        "\\r\\n" +
-        "Client time (UTC): ' + new Date().toISOString());" +
-        "</script>" +
-        "<noscript>Your client does not support JavaScript</noscript>",
-        "text/html"));
+    [EnableCors("AnyOrigin")] [ResponseCache(NoStore = true)]
+    () =>
+        Results.Text("<script>" +
+                     "window.alert('Your client supports JavaScript!" +
+                     "\\r\\n\\r\\n" +
+                     $"Server time (UTC): {DateTime.UtcNow.ToString("o")}" +
+                     "\\r\\n" +
+                     "Client time (UTC): ' + new Date().toISOString());" +
+                     "</script>" +
+                     "<noscript>Your client does not support JavaScript</noscript>",
+            "text/html"));
 
 // Controllers
 app.MapControllers().RequireCors("AnyOrigin");
